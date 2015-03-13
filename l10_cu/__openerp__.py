f15ecc32c5d49a4ac1c63bfdd83678244f8b67fe
ccarly <ccarly@gmail.com>
2015-03-13 11:14:08 -0500

    Update sumary

--- l10_cu/__openerp__.py
+++ l10_cu/__openerp__.py
@@ -6,10 +6,6 @@
     'category': 'Localization/Account Charts',
     'version': '0.1',
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
     'description': """
         Cuban Accounting and Tax Preconfiguration
     """,
