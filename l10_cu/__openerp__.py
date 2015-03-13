98f79f4f1af9235b1b436df5dc8389f45e381320
ccarly <ccarly@gmail.com>
2015-03-13 11:09:44 -0500

    Update __openerp.py__

--- l10_cu/__openerp__.py
+++ l10_cu/__openerp__.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 {
     'name': "l10_cu",
+    'author': "Carlos Fonseca",
+    'website': "https://www.fonse.net",
+    'category': 'Localization/Account Charts',
+    'version': '0.1',
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Cuban Accounting and Tax Preconfiguration
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'depends': [
+        'account',
+        'base_vat',
+        'account_chart',
+    ],
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
-        'demo.xml',
     ],
+    'installable': True,
+    'images': [],    
 }
